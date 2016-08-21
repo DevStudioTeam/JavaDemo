cfa925cf54e859139d5cdcfac0511c974eccdc18
Ilia Gromov <igromovbox@gmail.com>
2016-08-21 16:51:48 +0300

    Basic functionality added

--- src/main/java/com/igromov/simpleanagram/AnagramGame.java
+++ src/main/java/com/igromov/simpleanagram/AnagramGame.java
@@ -1,5 +1,27 @@
 package com.igromov.simpleanagram;
 
+import java.util.Scanner;
+
 public class AnagramGame {
-    
+
+    private final Dictionary dictionary;
+
+    public AnagramGame(Dictionary dictionary) {
+        this.dictionary = dictionary;
+    }
+
+    public void startLoop() {
+        Scanner scanner = new Scanner(System.in);
+        boolean isInterrupted = false;
+        while (!isInterrupted) {
+            System.out.println("> Press ENTER to get a new word, or type \"exit\"");
+            System.out.print("> ");
+            String cmd = scanner.next();
+            if (cmd.isEmpty()) {
+                
+            } else if (cmd.trim().toLowerCase().equals("exit")) {
+                System.exit(0);
+            }
+        }
+    }
 }
---
+++ src/main/java/com/igromov/simpleanagram/Dictionary.java
@@ -0,0 +1,6 @@
+package com.igromov.simpleanagram;
+
+public interface Dictionary {
+
+    void getAllWords();
+}
